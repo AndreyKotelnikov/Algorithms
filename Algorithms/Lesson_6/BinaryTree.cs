3953aed2383b5f7c3b04e365321d8d322fc9aee4
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-03-17 18:20:00 +0300

    2.Переписать программу, реализующее двоичное дерево поиска: a.Добавить в него обход дерева различными способами. b.Реализовать поиск в нём. + балансировка дерева.

--- Algorithms/Lesson_6/BinaryTree.cs
+++ Algorithms/Lesson_6/BinaryTree.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Lesson_6
 {
+    /// <summary>
+    /// Бинарное дерево
+    /// </summary>
     class BinaryTree
     {
         public Node Root { get; private set; }
@@ -19,9 +22,15 @@ namespace Lesson_6
             Height = 1;
         }
 
+        /// <summary>
+        /// Добавляет элемент в дерево, вычисляя сразу нужное место, чтобы сохранялось свойство: все элементы меньше слева, все больше - справа
+        /// </summary>
+        /// <param name="newNode">Узел, который нужно добавить в дерево</param>
+        /// <param name="nodeInTree">Текщий узел дерева, с которым сравниваем ключ нового узла</param>
+        /// <param name="depth">Текущия глубина (высота) дерева относительно корня</param>
         public void AddNode(Node newNode, Node nodeInTree = null, int depth = 2)
         {
-            if (nodeInTree == null) { nodeInTree = Root; CountNodes++; }
+            if (nodeInTree == null) { nodeInTree = Root; depth = 2; CountNodes++; }
             if (newNode.Data > nodeInTree.Data)
             {
                 if (nodeInTree.Right != null) { AddNode(newNode, nodeInTree.Right, depth + 1); }
@@ -44,6 +53,14 @@ namespace Lesson_6
             }
         }
 
+        /// <summary>
+        /// Выводим дерево в виде иерархической структуры - каждый новый уровень высоты на новой строчке консоли
+        /// </summary>
+        /// <param name="node">Узел, который нужно вывести в консоль</param>
+        /// <param name="depth">Текущая глубина (высота) на которой находится узел относительно корня</param>
+        /// <param name="count">Индекс узла по формуле: переход к левому сыну = count*2, переход к правому сыну = count*2 + 1</param>
+        /// <param name="xCursor">Позиция курсора по оси X перед началом вывода в консоль</param>
+        /// <param name="yCursor">Позиция курсора по оси Y перед началом вывода в консоль</param>
         public void Print(Node node = null, int depth = 1, int count = 1, int xCursor = 0, int yCursor = 0)
         {
             if (node == null)
@@ -58,7 +75,7 @@ namespace Lesson_6
             // Для вывода значения каждого узла выделяем длину в 4 позиций.
             if (Height == depth)
             {
-                Console.CursorLeft = (count - (1 << (depth - 1))) * 4;
+                Console.CursorLeft = xCursor + (count - (1 << (depth - 1))) * 4;
             }
             else
             {
@@ -69,6 +86,137 @@ namespace Lesson_6
             
             if (node.Left != null) { Print(node.Left, depth + 1, count * 2 , xCursor, yCursor); }
             if (node.Right != null) { Print(node.Right, depth + 1, count * 2 + 1, xCursor, yCursor); }
+            if (node == Root) { Console.CursorLeft = xCursor; Console.CursorTop = yCursor + Height + 1; }
+        }
+
+        /// <summary>
+        /// Рассчитывает максимальную высоту узла по правой и левой ветке
+        /// </summary>
+        /// <param name="node">Узел, для которого нужно рассчитать максимальную высоту</param>
+        /// <returns>Возвращает максимальную высоту узла по правой и левой ветке</returns>
+        public int MaxHeightOfNode(Node node)
+        {
+            if (node.Left != null && node.Right != null)
+            {
+                int a = MaxHeightOfNode(node.Left);
+                int b = MaxHeightOfNode(node.Right);
+                return a > b ? a + 1 : b + 1;
+            }
+            if (node.Left != null && node.Right == null) { return 1 + MaxHeightOfNode(node.Left); }
+            if (node.Right != null && node.Left == null) { return 1 + MaxHeightOfNode(node.Right); }
+            return 1;
+        }
+
+        /// <summary>
+        /// Делает одну ротацию узла вправо: левый сын становится родителем узла, правая ветка от левого сына переходит к левую ветку узла. 
+        /// </summary>
+        /// <param name="node">Узел, для которого нужно сделать ротацию со своим левым сыном</param>
+        public void RoteteRight(Node node)
+        {
+            Node left = node.Left;
+            if (node.Parent != null)
+            {
+                if (node.Parent.Left == node) { node.Parent.Left = left; }
+                else if (node.Parent.Right == node) { node.Parent.Right = left; }
+            }
+            else { Root = left; }
+            left.Parent = node.Parent;
+            node.Parent = left;
+            node.Left = left.Right;
+            left.Right = node;
+        }
+
+        /// <summary>
+        /// Делает одну ротацию узла влево: правый сын становится родителем узла, левая ветка от правого сына переходит к правую ветку узла. 
+        /// </summary>
+        /// <param name="node">Узел, для которого нужно сделать ротацию со своим левым сыном</param>
+        public void RoteteLeft(Node node)
+        {
+            Node right = node.Right;
+            if (node.Parent != null)
+            {
+                if (node.Parent.Left == node) { node.Parent.Left = right; }
+                else if (node.Parent.Right == node) { node.Parent.Right = right; }
+            }
+            else { Root = right; }
+            right.Parent = node.Parent;
+            node.Parent = right;
+            node.Right = right.Left;
+            right.Left = node;
+        }
+
+        /// <summary>
+        /// Проверяем для указанного узла разность высоты деревьев левого и правого сыновей, если она больше 1, то вызываем соответствующую ротацию узлов.
+        /// </summary>
+        /// <param name="node">Узел, ветви которого нужно сбалансировать</param>
+        public void BalanceTree(Node node = null)
+        {
+            bool flag = false;
+            if (node == null) { node = Root; flag = true; }
+            while (true)
+            {
+                if ((node.Left == null ? 0 : MaxHeightOfNode(node.Left)) - (node.Right == null ? 0 : MaxHeightOfNode(node.Right)) > 1)
+                {  RoteteRight(node);  }
+                else if ((node.Right == null ? 0 : MaxHeightOfNode(node.Right)) - (node.Left == null ? 0 : MaxHeightOfNode(node.Left)) > 1)
+                { RoteteLeft(node); }
+                else { break; }
+            }
+            if (node.Left != null) { BalanceTree(node.Left); }
+            if (node.Right != null) { BalanceTree(node.Right); }
+            if (Math.Abs((Root.Left == null ? 0 : MaxHeightOfNode(Root.Left)) - (Root.Right == null ? 0 : MaxHeightOfNode(Root.Right))) > 1)
+            { BalanceTree(Root); }
+            if (flag == true)
+            {
+               Height = MaxHeightOfNode(Root); 
+            }
+            return;
+        }
+
+        /// <summary>
+        /// Производит бинарный поиск по дереву и возвращает индекс элемента, который рассчитан по формуле: переход к левому сыну = index*2, переход к правому сыну = index*2 + 1
+        /// </summary>
+        /// <param name="data">Ключ, по которому нужно найти соответствующий узел</param>
+        /// <param name="node">Текущий узел для проверки совпадения ключа</param>
+        /// <param name="index">Индекс элемента, который рассчитан по формуле: переход к левому сыну = index*2, переход к правому сыну = index*2 + 1</param>
+        /// <returns>Возвращает индекс элемента, который рассчитан по формуле: переход к левому сыну = index*2, переход к правому сыну = index*2 + 1</returns>
+        private int BinarySearch(int data, Node node = null, int index = 1)
+        {
+            if (node ==null) { node = Root; index = 1; }
+            if (data == node.Data) { return index; }
+            if (data > node.Data)
+            {
+                if (node.Right != null) { return BinarySearch(data, node.Right, index * 2 + 1); }
+                else { return 0; }
+            }
+            if (data < node.Data)
+            {
+                if (node.Left != null) { return BinarySearch(data, node.Left, index * 2); }
+                else { return 0; }
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Интерфейс для организации работы с поиском по дереву через консоль: ввод значения от пользователя и вывод результата
+        /// </summary>
+        public void SearchInterface()
+        {
+            int data;
+            while (true)
+            {
+                Console.WriteLine("Укажите значение, которое нужно найти:");
+                if (int.TryParse(Console.ReadLine(), out data)) { break; }
+                else { Console.WriteLine("Указано некорректное значение, попробуйте ещё раз..."); }
+            }
+            int index = BinarySearch(data);
+            int str = (int)Math.Log(index, 2) + 1;
+            int pos = index - (1 << (str - 1)) + 1;
+
+            if (index != 0)
+            {
+                Console.WriteLine($"Элемент находится в {str} строке, на {pos} позиции слева, которые есть в этой строке.");
+            }
+            else { Console.WriteLine("Такой элемент отсутствует в нашем дереве."); }
         }
     }
 }
--- Algorithms/Lesson_6/Program.cs
+++ Algorithms/Lesson_6/Program.cs
@@ -14,48 +14,38 @@ namespace Lesson_6
             //2.Переписать программу, реализующее двоичное дерево поиска:
             //a.Добавить в него обход дерева различными способами.
             //b.Реализовать поиск в нём.
-            Node[] nodeList1 = new Node[7];
-            nodeList1[0] = new Node(4);
-            nodeList1[1] = new Node(2);
-            nodeList1[2] = new Node(1);
-            nodeList1[3] = new Node(3);
-            nodeList1[4] = new Node(6);
-            nodeList1[5] = new Node(5);
-            nodeList1[6] = new Node(7);
 
-            BinaryTree tree = new BinaryTree(nodeList1[0]);
-            for (int i = 1; i < nodeList1.Length; i++)
+            Console.SetBufferSize(300, 200); //Расширяем буфер вывода в консоль, чтобы поместилось отображение дерева с большой высотой. 
+
+            //Теперь создаём узлы для дерева
+            Random rand = new Random(30);
+            Node[] nodeList = new Node[7];
+            for (int i = 0; i < nodeList.Length; i++)
             {
-                tree.AddNode(nodeList1[i]);
+                nodeList[i] = new Node(rand.Next(100));
             }
-            Console.WriteLine($"Кол-во узлов = {tree.CountNodes}, высота = {tree.Height}, корень = {tree.Root.Data}");
+
+            //Создаём новое дерево и включаем в него все узлы
+            BinaryTree tree = new BinaryTree(nodeList[0]);
+            for (int i = 1; i < nodeList.Length; i++)
+            {
+                tree.AddNode(nodeList[i]);
+            }
+
+            //Выводим основные характеристики дерева и его структуру на экран
+            Console.WriteLine($"Кол-во узлов = {tree.CountNodes}, высота = {tree.Height}, корень = {tree.Root.Data}\n");
             tree.Print();
 
-            Node[] nodeList2 = new Node[16];
-            nodeList2[0] = new Node(8);
-            nodeList2[1] = new Node(4);
-            nodeList2[2] = new Node(12);
-            nodeList2[3] = new Node(2);
-            nodeList2[4] = new Node(6);
-            nodeList2[5] = new Node(10);
-            nodeList2[6] = new Node(14);
-            nodeList2[7] = new Node(1);
-            nodeList2[8] = new Node(3);
-            nodeList2[9] = new Node(5);
-            nodeList2[10] = new Node(7);
-            nodeList2[11] = new Node(9);
-            nodeList2[12] = new Node(11);
-            nodeList2[13] = new Node(13);
-            nodeList2[14] = new Node(15);
-            nodeList2[15] = new Node(16);
+            //Теперь балансируем дерево и выводим его основные характеристики и структуру на экран
+            Console.WriteLine("\n\nТеперь балансируем дерево:");
+            tree.BalanceTree();
+            Console.WriteLine($"Кол-во узлов = {tree.CountNodes}, высота = {tree.Height}, корень = {tree.Root.Data}\n");
+            tree.Print();
 
-            BinaryTree tree2 = new BinaryTree(nodeList2[0]);
-            for (int i = 1; i < nodeList2.Length; i++)
-            {
-                tree2.AddNode(nodeList2[i]);
-            }
-            Console.WriteLine($"\n\nКол-во узлов = {tree2.CountNodes}, высота = {tree2.Height}, корень = {tree2.Root.Data}");
-            tree2.Print();
+            //Теперь предлагаем пользователю указать значение, которое нужно найти в дереве
+            Console.WriteLine("\n\n");
+            tree.SearchInterface();
+            
 
             Console.ReadKey();
         }
