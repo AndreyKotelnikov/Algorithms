8ec7e159166b089397a6dc5312c9f8b706ec3186
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-03-20 21:48:04 +0300

    Создал класс Graf.

---
+++ Algorithms/Lesson_7/Graf.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    class Graf
+    {
+        private int[,] weightMatrix;
+
+        public Graf(int numberOfVertices)
+        {
+            weightMatrix = new int[numberOfVertices, numberOfVertices];
+        }
+
+        public void SetEdge(int firstVertice, int secondVertice, int weight, bool direction = false)
+        {
+            weightMatrix[firstVertice, secondVertice] = weight;
+            if (!direction) { weightMatrix[secondVertice, firstVertice] = weight; }
+        }
+
+        public void PrintMatrix()
+        {
+            Console.Write("   ");
+            for (int i = 0; i < weightMatrix.GetLength(0); i++)
+            {
+                Console.Write($"{i, 2} ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < weightMatrix.GetLength(1); i++)
+            {
+                Console.Write($"{i,2} ");
+                for (int j = 0; j < weightMatrix.GetLength(0); j++)
+                {
+                    Console.Write($"{(i == j ? "-" : weightMatrix[i, j].ToString()), 2} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Algorithms/Lesson_7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Андрей Котельников
+            //Реализовать алгоритм Дейкстры по обходу графа
+            //Создаём граф
+            Graf graf = new Graf(8);
+            graf.SetEdge(0, 1, 4);
+            graf.SetEdge(0, 2, 8);
+            graf.SetEdge(0, 3, 3);
+            graf.SetEdge(1, 5, 6);
+            graf.SetEdge(1, 4, 8);
+            graf.SetEdge(1, 2, 1);
+            graf.SetEdge(2, 4, 2);
+            graf.SetEdge(2, 3, 8);
+            graf.SetEdge(3, 6, 4);
+            graf.SetEdge(4, 5, 2);
+            graf.SetEdge(4, 7, 5);
+            graf.SetEdge(5, 7, 3);
+            graf.SetEdge(6, 7, 2);
+            graf.PrintMatrix();
+
+            //Проверяем граф на связанность
+            //Вычисляем минимальный путь между двумя вершинами по алгоритму Дейкстры
+            Console.ReadKey();
+        }
+    }
+}
